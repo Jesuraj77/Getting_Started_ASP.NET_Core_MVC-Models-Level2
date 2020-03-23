50a36cdc7b26fdac45c9c3ee2ab1bd3bab113427
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 16:51:39 +0530

    code added

---
+++ Level2/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Level2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<Product> Index()
+        {
+            List<Product> product = new List<Product>();
+            return product;
+        }
+    }
+}
---
+++ Level2/Repository/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Level2.Repository
+{
+    public class ProductRepository
+    {
+        public void get()
+        {
+            {
+                //implement code heree
+            }
+        }
+    }
+}
